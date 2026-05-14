6d4089fbaaa50b0fb52f29e44825da53d8b602ba
LeonardoTheGood <leonardo.cofone5@gmail.com>
2026-05-14 20:23:08 +0200

    v0.3.4

--- dsalt/training/trainer.py
+++ dsalt/training/trainer.py
@@ -261,7 +261,7 @@ class DSALTTrainer:
                 model,
                 device_ids=[local_rank],
                 output_device=local_rank,
-                find_unused_parameters=False,
+                find_unused_parameters=True,
                 gradient_as_bucket_view=True,
             )
 
